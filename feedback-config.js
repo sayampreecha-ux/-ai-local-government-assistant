e5593e66aabc056f50f49d496e14207488448c46
sayampreecha-ux <sayampreecha@gmail.com>
2026-07-26 06:53:57 +0700

    Update feedback-config.js with new endpoint example

--- feedback-config.js
+++ feedback-config.js
@@ -1,6 +1,5 @@
 /*
   ตั้งค่า URL ของ Google Apps Script Web App หลังทำตาม FEEDBACK_SETUP_GUIDE.txt
-  ตัวอย่าง: window.GOVPROMPT_FEEDBACK_ENDPOINT = 'https://script.google.com/macros/s/XXXXX/exec';
-*/
+  ตัวอย่าง: GOVPROMPT_FEEDBACK_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwcQDVl0B0unH-KK71xRqgqn21aWwHO-BYF6rR7U0fhRbmGS53ZCXbzhSih_CDp1s9O/exec';
 window.GOVPROMPT_FEEDBACK_ENDPOINT = '';
 window.GOVPROMPT_RELEASE_VERSION = 'v1.6-feedback';
