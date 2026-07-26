2e6c17b5c5d659ad72739657a4719bc3659cd809
sayampreecha-ux <sayampreecha@gmail.com>
2026-07-26 07:12:54 +0700

    Configure Google Apps Script for feedback
    Updated comments and set feedback endpoint and version.

--- feedback-config.js
+++ feedback-config.js
@@ -1,5 +1,5 @@
 /*
-  ตั้งค่า URL ของ Google Apps Script Web App หลังทำตาม FEEDBACK_SETUP_GUIDE.txt
-  ตัวอย่าง: GOVPROMPT_FEEDBACK_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwcQDVl0B0unH-KK71xRqgqn21aWwHO-BYF6rR7U0fhRbmGS53ZCXbzhSih_CDp1s9O/exec';
-window.GOVPROMPT_FEEDBACK_ENDPOINT = '';
+  ตั้งค่า Google Apps Script Web App สำหรับรับความคิดเห็น
+*/
+window.GOVPROMPT_FEEDBACK_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwcQDVl0B0unH-KK71xRqgqn21aWwHO-BYF6rR7U0fhRbmGS53ZCXbzhSih_CDp1s9O/exec';
 window.GOVPROMPT_RELEASE_VERSION = 'v1.6-feedback';
