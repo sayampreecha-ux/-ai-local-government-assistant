39fdcc2c9394b551449016349d7030d69689cfd5
sayampreecha-ux <sayampreecha@gmail.com>
2026-09-18 13:49:38 +0700

    fix: correct HTML evidence extraction regexes

--- src/search-worker.js
+++ src/search-worker.js
@@ -267,9 +267,9 @@ function normalizeResult(item) {
 
 function stripHtmlToEvidence(html, max = 12000) {
   return String(html || '')
-    .replace(/<script[\\s\\S]*?<\\/script>/gi, ' ')
-    .replace(/<style[\\s\\S]*?<\\/style>/gi, ' ')
-    .replace(/<noscript[\\s\\S]*?<\\/noscript>/gi, ' ')
+    .replace(/<script[^>]*>[\\s\\S]*?<\\/script>/gi, ' ')
+    .replace(/<style[^>]*>[\\s\\S]*?<\\/style>/gi, ' ')
+    .replace(/<noscript[^>]*>[\\s\\S]*?<\\/noscript>/gi, ' ')
     .replace(/<[^>]+>/g, ' ')
     .replace(/&nbsp;/gi, ' ')
     .replace(/&amp;/gi, '&')
